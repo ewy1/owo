b7a1b41c4005a535a5dd078acfaec3e53e93f596
Ewout Wieten <ewy0@protonmail.com>
2022-07-27 19:34:37 +0200

    make it gayer

--- owo.js
+++ owo.js
@@ -1,16 +1,53 @@
 import {EditorView, basicSetup} from "codemirror"
-import {oneDarkTheme} from "@codemirror/theme-one-dark";
 import {javascript} from "@codemirror/lang-javascript";
 
 const text = document.querySelector("#raw").textContent.slice(5, -6);
 const extensions = [basicSetup, EditorView.lineWrapping];
-const filename = document.querySelector("header").textContent;
+const filename = document.querySelector("input#filename").value;
+
+const darkTheme = EditorView.theme({
+  "&": {
+    color: "#ded5c8",
+    backgroundColor: "#312f2f"
+  },
+  ".cm-content": {
+    caretColor: "#ece7e7"
+  },
+  ".cm-activeLine": {
+    backgroundColor: "#1e1d1d",
+    caretColor: "#ffffff"
+  },
+  ".ͼe": {
+    color: "#edadad"
+  },
+  ".ͼb": {
+    color: "#bad1f9"
+  },
+  ".ͼg": {
+    color: "#bef8d7"
+  },
+  ".ͼl": {
+    color: "#fef5af"
+  },
+  ".ͼi": {
+    color: "#f2cae2"
+  }
+}, {dark: true});
 
 if (matchMedia("prefers-color-scheme: dark"))
-  extensions.push(oneDarkTheme);
+  extensions.push(darkTheme);
 
-if (filename.endsWith('.js') || filename.endsWith('.jsx'))
+if (filename.endsWith('.js'))
   extensions.push(javascript());
+
+if (filename.endsWith('.jsx'))
+  extensions.push(javascript({jsx: true}));
+
+if (filename.endsWith('.ts'))
+  extensions.push(javascript({typescript: true}));
+
+if (filename.endsWith('.tsx'))
+  extensions.push(javascript({typescript: true, jsx: true}));
 
 const editor = new EditorView({
   extensions: extensions,
