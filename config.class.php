61a1c20910bcaa4fa1211222a3993d5b9dcbff0b
Ewout <Ewout@voorraedt.nl>
2022-11-08 15:56:44 +0100

    Add default values

--- config.class.php
+++ config.class.php
@@ -4,15 +4,15 @@ class Config {
   /**
    * @var string Fully qualified domain name (or IP) of the webserver with protocol and trialing slash.
    */
-  public string $host;
+  public string $host = "https://localhost/";
 
   /**
    * @var string Path where the raw files will be stored, with trailing path separator.
    */
-  public string $pastePath;
+  public string $pastePath = "/var/www/owo";
 
   /**
    * @var string The default title of a paste and the tab.
    */
-  public string $defaultTitle;
+  public string $defaultTitle = "owo *notices ur pasting*";
 }
